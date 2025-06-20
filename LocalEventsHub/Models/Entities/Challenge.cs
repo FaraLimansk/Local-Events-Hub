083e2358a722f14716214ad99d9ea02ed7466b87
Ivan <faralimansk7777@mail.ru>
2025-06-20 21:22:02 +0300

    Очистка репозитория от всех файлов

--- LocalEventsHub/Models/Entities/Challenge.cs
+++
@@ -1,9 +0,0 @@
-namespace LocalEventsHub.Models.Entities
-{
-    public class Challenge
-    {
-        public int ChallengeId { get; set; }
-        public string Title { get; set; } = null!;
-        public string Description { get; set; } = null!;
-    }
-}
